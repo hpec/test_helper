5a95e8f581cc23b6731d55f15e77589d22fc5f38
Daniel Liu <hpec@qq.com>
2015-05-10 22:12:09 -0700

    minor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   version = '0.1',
   description = 'A testing helper for scalable machine learning mooc',
   author = 'Daniel Liu',
-  author_email = 'hpec@qq.com',
+  author_email = 'hpec.liu@gmail.com',
   url = 'https://github.com/hpec/test_helper',
   download_url = 'https://github.com/hpec/test_helper/tarball/0.1',
   keywords = ['testing', 'autograder', 'mooc'],
