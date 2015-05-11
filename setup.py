828d3538da100743cd2032e15a5d0f1271c4a064
Daniel Liu <hpec@qq.com>
2015-05-10 21:58:03 -0700

    prepare for pypi submission

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'test_helper',
+  packages = ['test_helper'],
+  version = '0.1',
+  description = 'A testing helper for scalable machine learning mooc',
+  author = 'Daniel Liu',
+  author_email = 'hpec@qq.com',
+  url = 'https://github.com/hpec/test_helper',
+  download_url = 'https://github.com/hpec/test_helper/tarball/0.1',
+  keywords = ['testing', 'autograder', 'mooc'],
+  classifiers = [],
+)
